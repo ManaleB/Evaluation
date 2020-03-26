d05c281e1f4c7ff60fbf134d7f84a236c2ae0827
ManaleB <62698051+ManaleB@users.noreply.github.com>
2020-03-26 14:34:05 +0000

    commit

---
+++ controle_cours.py
@@ -0,0 +1,34 @@
+def sup21(n):
+  if n < 21 : 
+    print("False")
+  else :
+    print("True")
+  return
+
+
+def pairs(l):
+
+
+
+
+
+
+def aller_a_paris(input_call=input):
+    # code a remplir
+
+    # quelque part dans le code de cette fonction: saisie = input_call('Question ')
+    # en fonction de saisie on continue a demander ou on renvoie 'Paris'
+    # Au lieu d'utiliser input comme en cours vous appelez input_call
+    # par défaut elle vaut input donc vous pouvez appeller
+    # aller_a_paris() pour tester a la main
+    while True:
+        return 0, 'Nulle Part'
+      
+if __name__ == "__main__":
+    import doctest
+    if True:
+        doctest.testmod(verbose=True, optionflags=512)
+    else:
+        doctest.testmod(verbose=True)
+
+
