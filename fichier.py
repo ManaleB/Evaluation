e4bb8380101b41853115e2bacb56608338b3dd02
ManaleB <62698051+ManaleB@users.noreply.github.com>
2020-03-26 12:28:33 +0000

    commit

--- fichier.py
+++ fichier.py
@@ -2,14 +2,14 @@
 print("Combien d'anagrammes possibles ?")
 
 
-def listeAnagramme(mot) :
+def liste_anagrammes(mot) :
 	if len(mot)==1 : return [mot]
 
 	liste=[]
-	for anagr in listeAnagramme(mot[1:]) :
+	for anagr in liste_anagrammes(mot[1:]) :
 		for k in range(len(mot)) :
 			liste.append(anagr[:k]+mot[0]+anagr[k:])
 	return liste
 	
 
-print(listeAnagramme('abc'))
+print(liste_anagrammes("abc"))
